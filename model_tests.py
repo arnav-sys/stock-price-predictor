1b692c0e0dfd410108bd942245a57f3db5c58724
arnav-sys <74288659+arnav-sys@users.noreply.github.com>
2022-01-12 18:46:20 +0530

    Update model_tests.py

--- model_tests.py
+++ model_tests.py
@@ -13,10 +13,38 @@ Created on Sat Jan  8 18:05:15 2022
 
 import joblib
 import pandas as pd
-from model/preprocess_data import process_data
 from sklearn.model_selection import train_test_split
 import numpy as np
 from sklearn.linear_model import LinearRegression
+
+from sklearn.preprocessing import MinMaxScaler, OrdinalEncoder
+from sklearn.compose import ColumnTransformer
+
+
+def process_data(df):
+    scaler = MinMaxScaler()
+    encoder = OrdinalEncoder()
+    
+    data2 = pd.read_csv("./data/NIFTY50_all.csv")
+    
+    data2["Year"] = data2["Date"].apply(lambda x: int(x[0:4]))
+    data2["Month"] = data2["Date"].apply(lambda x: int(x[5:7]))
+    data2["Day"] = data2["Date"].apply(lambda x: int(x[8:10]))
+    
+    num_data =["Open","High","Low","Last","VWAP","Volume","Turnover","Month","Day","Year","Prev Close"]
+    cat_data = ["Date","Symbol"]
+    
+    pipeline = ColumnTransformer([
+        ("num", MinMaxScaler(), num_data),
+        ("cat",OrdinalEncoder(),cat_data)
+    ])
+    
+    data2 = pipeline.fit_transform(data2)
+    
+    df = pd.DataFrame(pipeline.transform(df))
+
+    
+    return df
 
 
 # Load model as a Spark UDF.
